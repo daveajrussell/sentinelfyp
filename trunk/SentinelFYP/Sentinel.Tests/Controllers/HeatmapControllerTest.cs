1f0c80879581a768914f65e5b587bade0c6a5a3b
dave.aj.russell <dave.aj.russell@6fd2dcf3-2286-413c-94f7-06109e031aca>
2013-02-14 00:07:06 +0000

    13/02/13  - Added new unit tests

--- trunk/SentinelFYP/Sentinel.Tests/Controllers/HeatmapControllerTest.cs
+++ trunk/SentinelFYP/Sentinel.Tests/Controllers/HeatmapControllerTest.cs
@@ -10,74 +10,95 @@ using System.Web.Mvc;
 using GMap.NET;
 using DomainModel.Test.TestHelpers;
 using Sentinel.Helpers.ExtensionMethods;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DomainModel.Models;
 using System.Drawing;
 using System.Drawing.Imaging;
 using Sentinel.Controllers;
 using Sentinel.Tests.TestHelpers;
+using DomainModel.Models.GISModels;
 
 namespace Sentinel.Tests.Controllers
 {
-    //[TestClass]
     public class HeatmapControllerTest
     {
         private Mock<IPointService> _pointService;
         private Mock<IGHeatService> _gheatService;
+        private Mock<ILiveTrackingService> _trackingService;
 
         public HeatmapControllerTest()
         {
             _pointService = new Mock<IPointService>();
             _gheatService = new Mock<IGHeatService>();
+            _trackingService = new Mock<ILiveTrackingService>();
 
             _gheatService.Setup(m => m.GetTile(It.IsAny<List<PointLatLng>>(), It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>()))
-                .Returns<List<PointLatLng>, string, int, int, int>((points, colorScheme, zoom, x, y) => SentinelTestHelper.GetTileMock(points, colorScheme, zoom, x, y));
+                .Returns<List<PointLatLng>, string, int, int, int>((points, colorScheme, zoom, x, y) => TrackingTestHelper.GetTileMock(points, colorScheme, zoom, x, y));
 
             _pointService.Setup(m => m.LoadPoints())
                 .Returns(new List<PointLatLng>() { new PointLatLng(12, 12) });
+
+            _trackingService.Setup(m => m.GetAllLiveElapsedRoutes())
+                .Returns(() => TrackingTestHelper.MockGetAllLiveElapsedRoutes());
         }
 
         [Fact]
         public void InjectingNullPointServiceIntoConstructorShouldThrow()
         {
-            Xunit.Assert.Throws<ArgumentNullException>(() => new HeatmapController(null, _gheatService.Object, null));
+            Assert.Throws<ArgumentNullException>(() => new HeatmapController(null, null, null));
         }
 
         [Fact]
         public void InjectingNullGHeatServiceIntoConstructorShouldThrow()
         {
-            Xunit.Assert.Throws<ArgumentNullException>(() => new HeatmapController(_pointService.Object, null, null));
+            Assert.Throws<ArgumentNullException>(() => new HeatmapController(_pointService.Object, null, null));
+        }
+
+        [Fact]
+        public void InjectingNullTrackingServiceShouldThrow()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HeatmapController(_pointService.Object, _gheatService.Object, null));
         }
 
         [Fact]
         public void ControllerConstructorShouldNotThrow()
         {
-            Xunit.Assert.DoesNotThrow(() => new HeatmapController(_pointService.Object, _gheatService.Object, null));
+            Assert.DoesNotThrow(() => new HeatmapController(_pointService.Object, _gheatService.Object, _trackingService.Object));
         }
 
         [Fact]
         public void IndexReturnsActionResult()
         {
-            var controller = new HeatmapController(_pointService.Object, _gheatService.Object, null);
+            var controller = new HeatmapController(_pointService.Object, _gheatService.Object, _trackingService.Object);
             ActionResult result = null;
 
-            Xunit.Assert.DoesNotThrow(() => result = controller.Index());
-            Xunit.Assert.NotNull(result);
-            Xunit.Assert.IsAssignableFrom<ActionResult>(result);
+            Assert.DoesNotThrow(() => result = controller.Index());
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<ActionResult>(result);
         }
 
         [Fact]
-        //[TestMethod]
         public void GetTileReturnsTileResult()
         {
-            var controller = new HeatmapController(_pointService.Object, _gheatService.Object, null);
+            var controller = new HeatmapController(_pointService.Object, _gheatService.Object, _trackingService.Object);
             TileResult result = null;
 
-            //result = controller.Tile("classic", "5", "12", "12", new Random().Next().ToString());
+            Assert.DoesNotThrow(() => result = controller.Tile("classic", "5", "12", "12", new Random().Next().ToString()));
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<TileResult>(result);
+        }
 
-            Xunit.Assert.DoesNotThrow(() => result = controller.Tile("classic", "5", "12", "12", new Random().Next().ToString()));
-            Xunit.Assert.NotNull(result);
-            Xunit.Assert.IsAssignableFrom<TileResult>(result);
+        [Fact]
+        public void TestGetAllLiveElapsedRoutes()
+        {
+            var controller = new HeatmapController(_pointService.Object, _gheatService.Object, _trackingService.Object);
+            ActionResult result = null;
+
+            Assert.DoesNotThrow(() => result = controller.LiveHeatmapData());
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<PartialViewResult>(result);
+
+            Assert.NotNull(((ViewResultBase)(result)).Model);
+            Assert.IsAssignableFrom<IEnumerable<ElapsedGeospatialInformation>>(((ViewResultBase)(result)).Model);
         }
     }
 }
