9f624e28936237bf18bc8769e17ea04d3df99b37
dave.aj.russell <dave.aj.russell@6fd2dcf3-2286-413c-94f7-06109e031aca>
2013-02-14 00:06:00 +0000

    13/02/13 - Services had changed, tests were defunct. - New tests to be written shortly.

--- trunk/SentinelFYP/WebServices.Test/Tests/LocationServiceTests.cs
+++ trunk/SentinelFYP/WebServices.Test/Tests/LocationServiceTests.cs
@@ -17,16 +17,22 @@ namespace WebServices.Test.Tests
             client = new LocationServiceClient();
         }
 
+
+
+
+
+        /*
         [Fact]
         public void PassingNonJSONStringToServiceShouldThrow()
         {
-            Xunit.Assert.Throws<FaultException<ExceptionDetail>>(() => client.PostGISData("Daft"));
+            Assert.Throws<FaultException<ExceptionDetail>>(() => client.PostGISData("Daft"));
         }
 
         [Fact]
         public void PassingJSONStringToServiceShouldNotThrow()
         {
-            Xunit.Assert.DoesNotThrow(() => client.PostGISData("{\"lngTimeStamp\":1355267165704,\"oUserIdentification\":\"00000000-0000-0000-0000-000000000000\",\"dLatitude\":37.422005,\"dLongitude\":-122.084095,\"intOrientation\":1,\"dSpeed\":0}"));
+            Assert.DoesNotThrow(() => client.PostGISData("{\"lngTimeStamp\":1355267165704,\"oUserIdentification\":\"00000000-0000-0000-0000-000000000000\",\"dLatitude\":37.422005,\"dLongitude\":-122.084095,\"intOrientation\":1,\"dSpeed\":0}"));
         }
+        */ 
     }
 }
